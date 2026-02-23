567d2a17f9248aa9923b542028f6e4f8b42cccc3
alias-ktc <alenpeni.info@gmail.com>
2026-02-23 13:55:04 +1000

    Added settings paramter
    The settings kwarg is used to specify how to organize the directory
    based on a directory-extensions mapping

--- _main.py
+++ _main.py
@@ -6,5 +6,5 @@ This assumes the library is installed in edit mode
 from organize_fd import Organizer
 
 if __name__ == "__main__":
-    organizer = Organizer()
-    print(organizer.say_hi())
+    organizer = Organizer(settings={"Documents": [".pdf"]})
+    organizer.print_settings()
--- src/organize_fd/_organizer.py
+++ src/organize_fd/_organizer.py
@@ -1,3 +1,20 @@
+Organizer_Settings = dict[str, list[str]]
+
+
 class Organizer:
-    def say_hi(self) -> str:
-        return "Hello"
+    """Used to create Organizer instances to organize a directory."""
+
+    def __init__(self, settings: Organizer_Settings) -> None:
+        """Instantiate the organizer with the given mapping.
+
+        Args:
+            settings (Organizer_Settings): A dictionary where the keys
+            are the name of the directory and the values are lists of
+            file extensions
+        """
+        self.settings = settings
+
+    def print_settings(self) -> None:
+        """Helper method to display what files will be moved to which directory."""
+        for destination, extension_list in self.settings.items():
+            print(f"{destination} will hold:\n{extension_list}")
